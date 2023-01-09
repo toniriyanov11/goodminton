271f8f5a9971ff46ac2019f94a09d0e3cbbeb1e8
Toni Rizky  Riyanov <toni.rizky@bpjsketenagakerjaan.go.id>
2023-01-09 22:12:37 +0700

    style: fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,23 +5,23 @@ function Footer(){
     return(
         <footer className="bg-white rounded-tr-[8rem] px-6 py-14 text-left -mt-[8rem] text-black relative z-4 drop-shadow-xl">
             <img src={logo} alt='logo'></img>
-            <p className='my-12 w-4/5'>
+            <p className='mb-12 mt-8 w-4/5'>
                 Komunitas bermain badminton tanpa perlu pusing booking lapangan dan shuttlecock. Cukup persiapkan raket kamu dan mari bersenang-senang.
             </p>
             <div className='flex flex-row my-12 leading-[2.8rem]'>
-                <div>
-                    <p className='font-semibold text-sm'>COMMUNTIY</p>
-                    <p>About Us</p>
-                    <p>Gallery</p>
-                    <p>Testimonial</p>
+                <div className='flex flex-col'>
+                    <p className='font-semibold text-sm mb-4'>Community</p>
+                    <a href='#about-us'>About Us</a>
+                    <a href='#gallery'>Gallery</a>
+                    <a href='#testimonial'>Testimonial</a>
                 </div>
                 <div className='mx-9'>
-                    <p className='font-semibold text-sm'>GET IN TOUCH</p>
+                    <p className='font-semibold text-sm mb-4'>Get In Touch</p>
                     <p>Instagram</p>
                 </div>
             </div>
             <div>
-                COPYRIGHT © 2023 — <b>riyanov</b>studio.
+                <p className='text-sm'>Copyright © 2023 — <a href='http://riyanovstudio.com'><strong>riyanov</strong>studio.</a></p>
             </div>
         </footer>
     )
